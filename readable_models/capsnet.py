389542586546bee0875549669a8ebbee7b8f27db
Alex Rogozhnikov <iamfullofspam@gmail.com>
2020-09-12 02:45:33 -0700

    initial version for capsnets

---
+++ readable_models/capsnet.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+
+from einops import reduce, repeat, rearrange
+from einops.layers.torch import WeightedEinsum, Rearrange
+
+
+def squash(x, dim):
+    """
+    The non-linear activation used in Capsule.
+    It drives the length of a large vector to near 1 and small vector to 0
+    This implement equation 1 from the paper.
+    """
+    norm_sq = torch.sum(x ** 2, dim, keepdim=True)
+    norm = torch.sqrt(norm_sq)
+    return (norm_sq / (1.0 + norm_sq)) * (x / norm)
+
+
+class Capsule2Capsule(nn.Module):
+    def __init__(self, in_caps, in_hid, out_caps, out_hid, routing_iterations=3):
+        super().__init__()
+        assert routing_iterations > 0, 'at least one iteration is needed'
+        self.routing_iterations = routing_iterations
+        self.input_caps2U = WeightedEinsum(
+            'b in_caps in_hid -> b in_caps out_caps out_hid',
+            weight_shape='in_caps in_hid out_caps out_hid',
+            in_hid=in_hid, in_caps=in_caps, out_hid=out_hid, out_caps=out_caps,
+        )
+
+    def forward(self, input_capsules):
+        U = self.input_caps2U(input_capsules)
+
+        batch, in_caps, out_caps, out_hid = U.shape
+
+        # logsoftmax for connections between capsules
+        B = torch.zeros([batch, in_caps, out_caps])
+
+        # routing algorithm
+        # names of axes: b=batch, i=input capsules, o=output_capsules, h=hidden dim of output capsule
+        for _ in range(self.routing_iterations):
+            # "routing softmax" that determines connection between capsules in layers
+            C = torch.softmax(B, dim=-1)
+            S = torch.einsum('bio,bioh->boh', C, U)
+            V = squash(S, dim=-1)
+            B = B + torch.einsum('bioh,boh->bio', U, V)
+        assert torch.norm(V, dim=2).max() <= 1.001
+        return V
+
+
+class RoutingEncoder(nn.Module):
+    def __init__(self, in_h, in_w, in_c,
+                 n_primary_caps_groups, primary_caps_dim,
+                 n_digit_caps, digit_caps_dim,
+                 ):
+        super().__init__()
+        self.image2primary_capsules = nn.Sequential(
+            nn.Conv2d(in_c, 256, kernel_size=9),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, n_primary_caps_groups * primary_caps_dim, kernel_size=9, stride=2),
+            Rearrange('b (caps hid) h w -> b (h w caps) hid', caps=n_primary_caps_groups, hid=primary_caps_dim),
+        )
+        # figure out correct number of capsules by passing a test image through
+        _, self.n_primary_capsules, _ = self.image2primary_capsules(torch.zeros(1, in_c, in_h, in_w)).shape
+        self.primary2digit_capsules = Capsule2Capsule(
+            in_caps=self.n_primary_capsules, in_hid=primary_caps_dim,
+            out_caps=n_digit_caps, out_hid=digit_caps_dim,
+        )
+
+    def forward(self, images):
+        primary_capsules = self.image2primary_capsules(images) * 0.01
+        return self.primary2digit_capsules(primary_capsules)
+
+
+def Decoder(n_caps, caps_dim, output_h, output_w, output_channels):
+    return nn.Sequential(
+        WeightedEinsum('b caps caps_dim -> b hidden', weight_shape='caps caps_dim hidden',
+                       caps=n_caps, caps_dim=caps_dim, hidden=512),
+        nn.ReLU(inplace=True),
+        nn.Linear(512, 1024),
+        nn.ReLU(inplace=True),
+        WeightedEinsum('b hidden -> b c h w', weight_shape='hidden c h w',
+                       hidden=1024, h=output_h, w=output_w, c=output_channels),
+        nn.Sigmoid(),
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+def test(n_digit_caps=10, digit_caps_dim=11):
+    size = 28
+    image_channels = 3
+    encoder = RoutingEncoder(
+        in_c=image_channels, in_h=size, in_w=size,
+        n_primary_caps_groups=image_channels,
+        primary_caps_dim=12,
+        n_digit_caps=n_digit_caps,
+        digit_caps_dim=digit_caps_dim,
+    )
+
+    batch_size = 2
+    images = torch.zeros(batch_size, image_channels, size, size)
+
+    embeddings = encoder(images)
+
+    assert embeddings.shape == (batch_size, n_digit_caps, digit_caps_dim)
+
+    decoder = Decoder(n_digit_caps, caps_dim=digit_caps_dim, output_h=size, output_w=size,
+                      output_channels=image_channels)
+    recostructed = decoder(embeddings)
+
+    assert recostructed.shape == (batch_size, image_channels, size, size)
+    print('ok')
+
+
+test()
---
+++ readable_models/train.py
@@ -0,0 +1,76 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+from einops import reduce, asnumpy
+import numpy as np
+
+# Preparing dataset
+
+
+def load_mnist(batch_size, workers=4):
+    # Normalize MNIST dataset.
+    train_transform = transforms.Compose([
+        transforms.ToTensor(),
+        # transforms.RandomAffine(degrees=(0, 0), translate=(0.1, 0.1), fillcolor=0),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    kwargs = dict(num_workers=workers, pin_memory=True)
+
+    training_data_loader = DataLoader(
+        datasets.MNIST('./data', train=True, download=True, transform=train_transform),
+        batch_size=batch_size, shuffle=True, **kwargs)
+
+    testing_data_loader = DataLoader(
+        datasets.MNIST('./data', train=False, download=True, transform=test_transform),
+        batch_size=batch_size, shuffle=True, **kwargs)
+
+    return training_data_loader, testing_data_loader
+
+
+from capsnet import RoutingEncoder, Decoder
+
+encoder = RoutingEncoder(
+    in_h=28, in_w=28, in_c=1,
+    n_primary_caps_groups=32, primary_caps_dim=8,
+    n_digit_caps=10, digit_caps_dim=16
+)
+
+optim = torch.optim.Adam(encoder.parameters())
+train_loader, test_loader = load_mnist(batch_size=64, workers=4)
+device = torch.device('cpu')
+
+
+def margin_loss(class_capsules, target_one_hot, m_minus=0.1, m_plus=0.9, loss_lambda=0.5):
+    caps_norms = torch.norm(class_capsules, dim=2)
+    assert caps_norms.max() <= 1.001
+    # Calculate left and right max() terms.
+    loss_sig = torch.clamp(m_plus - caps_norms, 0) ** 2
+    loss_bkg = torch.clamp(caps_norms - m_minus, 0) ** 2
+
+    t_c = target_one_hot
+    # Lc is margin loss for each digit of class c
+    loss = t_c * loss_sig + loss_lambda * (1.0 - t_c) * loss_bkg
+    return reduce(loss, 'b c -> b', 'sum')
+
+
+for epoch in range(6):
+    for i, (images, labels) in enumerate(train_loader):
+        digit_capsules = encoder(images.to(device))
+        labels_one_hot = torch.nn.functional.one_hot(labels).to(device)
+        loss = margin_loss(digit_capsules, labels_one_hot).mean()
+        loss.backward()
+        optim.step()
+        optim.zero_grad()
+        print(epoch, i, loss.item())
+
+    accuracies = []
+    for images, labels in test_loader:
+        digit_capsules = encoder(images.to(device))
+        predicted_labels = digit_capsules.norm(dim=2).argmax(dim=1)
+        accuracies += asnumpy(predicted_labels == labels).tolist()
+
+    print(f'epoch {epoch} accuracy: {np.mean(accuracies)}')
